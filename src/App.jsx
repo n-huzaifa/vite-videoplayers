18c0410d72c2d434c829d2cbdb97498295e3fc93
n-huzaifa <noorhuzaifa11@gmail.com>
2023-01-05 17:27:51 +0500

    Video Js and React Player Integration and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import Videoplayer from "./components/Videoplayer";
+import "./styles/App.css";
 
 function App() {
-  return <ReactPlayer url='/1080p.mp4' controls={true} />;
+  return (
+    <div className='main'>
+      <div className='main-body'>
+        <h1>Video JS Integration Test</h1>
+        <Videoplayer />
+        <br />
+        <h1>React Player Integration Test</h1>
+        <ReactPlayer url='/720p.mp4' controls />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Videoplayer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import VideoJS from "../lib/VideoJS";
+
+function Videoplayer() {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: false,
+    controls: true,
+    width: "1080",
+    height: "608",
+    sources: [
+      {
+        src: "/720p.mp4",
+        type: "video/mp4",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
+  return <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />;
+}
+
+export default Videoplayer;
